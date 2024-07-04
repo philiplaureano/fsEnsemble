24aeb44553379b686c47bd36aafdf4705a0a2c7c
Philip Laureano <philip.laureano@gmail.com>
2024-07-04 23:57:23 +1000

    Added the fsScratch and csScratch projects, which allow for throwaway experimentation without checking anything in

---
+++ csScratch/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace csScratch;
+
+// Note: This project is for experimentation purposes only and will never be checked in.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Scratch World!");
+    }
+}
---
+++ fsScratch/Program.fs
@@ -0,0 +1,2 @@
+﻿// Note: This project is for experimentation purposes only and will never be checked in.
+printfn "Hello, Scratch World!"
